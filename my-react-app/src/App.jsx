4078845f362be6a82ba2ba3bc64275c37e0f8583
Getonto3 <getontomoses18@gmail.com>
2024-01-25 22:08:09 +0300

    feat: created a component to handle props

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -1,8 +1,10 @@
 import React from "react";
+import PropsLookup from "./PropsLookup";
 
 export default function App() {
   return (
     <div>
+       <PropsLookup comment="awesome" /> {/*I can pass a prop to the component in another component */}
       <h1>Hello, world! from the first component</h1>
     </div>
   );
---
+++ my-react-app/src/PropsLookup.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function PropsLookup(props) {
+  return (
+    <div>
+      <h1>React is {props.comment}</h1>
+    </div>
+  );
+}
