f27e6a4c247b2047c99dab7c56aa060c5e13ab8e
Getonto3 <getontomoses18@gmail.com>
2024-01-26 16:45:37 +0300

    chore: comments for the login form

--- my-react-app/src/Login.jsx
+++ my-react-app/src/Login.jsx
@@ -4,14 +4,17 @@ export default function Login() {
   const [input, setInputs] = useState({});
 
   const handleNameChange = (event) => {
-    const name = event.target.name;
+    const name = event.target.name; //what is the event? the event is onChange .
+    // and where is the target? the target is the input element in our html code in return.
+    // and what is the name? the name is the name attribute of the input element in our html code
     const value = event.target.value;
     setInputs((values) => ({ ...values, [name]: value }));
   };
 
   const handleSubmit = (event) => {
     event.preventDefault();
-    alert(`The name you entered was: ${input.username}`);
+    alert(`The name you entered was: ${input.username}`); //input.username refers to the name attribute
+    //of the input element in our html code
   };
   return (
     <>
