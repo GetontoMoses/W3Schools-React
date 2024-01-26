f12049933bffa88acde4e651a46b2632d3416d96
Getonto3 <getontomoses18@gmail.com>
2024-01-26 15:11:33 +0300

    feat: component to implement forms

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -1,11 +1,15 @@
 import React from "react";
 import PropsLookup from "./PropsLookup";
+import Forms from "./Forms";
 
 export default function App() {
   return (
-    <div>
-       <PropsLookup comment="awesome" /> {/*I can pass a prop to the component in another component */}
-      <h1>Hello, world! from the first component</h1>
-    </div>
+    <><Forms/>
+      <div>
+        <PropsLookup comment="awesome" />{" "}
+        {/*I can pass a prop to the component in another component */}
+        <h1>Hello, world! from the first component</h1>
+      </div>
+    </>
   );
 }
