ec59fdd3cf5c29c7a8bf4593a2b0112822beaf65
Getonto3 <getontomoses18@gmail.com>
2024-01-29 21:45:57 +0300

    feat: layout component for the project

---
+++ my-react-app/src/Layout.jsx
@@ -0,0 +1,14 @@
+import { Outlet, Link } from "react-router-dom";
+
+export default function Layout() {
+  return (
+    <>
+      <h1>My App</h1>
+      <Link to="/">Home</Link>
+      <div>
+        <Link to="contact">Contact</Link>
+      </div>
+      <Outlet />
+    </>
+  );
+}
